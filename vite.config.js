5fe1c3ee29013689557c540f8af2d5f8e1cca477
Justin Renjilian <jrenjilian@overdrive.com>
2023-01-27 12:38:30 -0500

    Add resolve.dedupe entries

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+/** @type {import("vite").UserConfig} */
 export default {
   resolve: {
     alias: {
       "/real.js": "/mock",
     },
+    // None of these seem to work:
+    dedupe: [
+      "/mock.js",
+      "mock.js",
+      "/mock",
+      "mock",
+      "/real.js",
+      "real.js",
+      "/real",
+      "real",
+    ],
   },
 };
